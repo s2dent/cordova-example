bbad9cd12c7020f923413563d3df5f1650afb609
s2dent <s2dentm@gmail.com>
2017-11-24 23:29:51 +0200

    update

--- www/js/index.js
+++ www/js/index.js
@@ -1,105 +1,49 @@
-// (c) 2014 Don Coleman
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-/* global mainPage, deviceList, refreshButton */
-/* global detailPage, batteryState, batteryStateButton, disconnectButton */
-/* global ble  */
-/* jshint browser: true , devel: true*/
-'use strict';
-
-var battery = {
-    service: "180F",
-    level: "2A19"
-};
-
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 var app = {
+    // Application Constructor
     initialize: function() {
         this.bindEvents();
-        detailPage.hidden = true;
     },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        refreshButton.addEventListener('touchstart', this.refreshDeviceList, false);
-        batteryStateButton.addEventListener('touchstart', this.readBatteryState, false);
-        disconnectButton.addEventListener('touchstart', this.disconnect, false);
-        deviceList.addEventListener('touchstart', this.connect, false); // assume not scrolling
     },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.refreshDeviceList();
+        app.receivedEvent('deviceready');
     },
-    refreshDeviceList: function() {
-        deviceList.innerHTML = ''; // empties the list
-        // scan for all devices
-        ble.scan([], 5, app.onDiscoverDevice, app.onError);
-    },
-    onDiscoverDevice: function(device) {
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
 
-        console.log(JSON.stringify(device));
-        var listItem = document.createElement('li'),
-            html = '<b>' + device.name + '</b><br/>' +
-                'RSSI: ' + device.rssi + '&nbsp;|&nbsp;' +
-                device.id;
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
 
-        listItem.dataset.deviceId = device.id;  // TODO
-        listItem.innerHTML = html;
-        deviceList.appendChild(listItem);
-
-    },
-    connect: function(e) {
-        var deviceId = e.target.dataset.deviceId,
-            onConnect = function() {
-
-                // TODO check if we have the battery service
-                // TODO check if the battery service can notify us
-                //ble.startNotification(deviceId, battery.service, battery.level, app.onBatteryLevelChange, app.onError);
-                batteryStateButton.dataset.deviceId = deviceId;
-                disconnectButton.dataset.deviceId = deviceId;
-                app.showDetailPage();
-            };
-
-        ble.connect(deviceId, onConnect, app.onError);
-    },
-    onBatteryLevelChange: function(data) {
-        console.log(data);
-        var message;
-        var a = new Uint8Array(data);
-        batteryState.innerHTML = a[0];
-    },
-    readBatteryState: function(event) {
-        console.log("readBatteryState");
-        var deviceId = event.target.dataset.deviceId;
-        ble.read(deviceId, battery.service, battery.level, app.onReadBatteryLevel, app.onError);
-    },
-    onReadBatteryLevel: function(data) {
-        console.log(data);
-        var message;
-        var a = new Uint8Array(data);
-        batteryState.innerHTML = a[0];
-    },
-    disconnect: function(event) {
-        var deviceId = event.target.dataset.deviceId;
-        ble.disconnect(deviceId, app.showMainPage, app.onError);
-    },
-    showMainPage: function() {
-        mainPage.hidden = false;
-        detailPage.hidden = true;
-    },
-    showDetailPage: function() {
-        mainPage.hidden = true;
-        detailPage.hidden = false;
-    },
-    onError: function(reason) {
-        alert("ERROR: " + reason); // real apps should use notification.alert
+        console.log('Received Event: ' + id);
     }
 };
